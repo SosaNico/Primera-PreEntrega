ba489df08288ef2e6e58295d3113c14018eb98c9
Nico Sosa <sosanicolasdaniel00@gmail.com>
2022-06-29 23:31:58 -0300

    arrays complementario

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 // SIMULADOR INTERACTIVO
-
-//VARIABLES
-
+class Producto {
+    constructor(marca, modelo, estado) {
+        this.marca = marca.toUpperCase();
+        this.modelo = modelo;
+        this.estado = estado;
+    }
+}
+let infoCotizacion = [];
 let nombreCliente = userName();
 let marcaAutomotor = mostrarMenu();
 let dinero = precios(marcaAutomotor);
 cotizacion(marcaAutomotor, dinero, nombreCliente);
 formaPago();
 
-// FUNCIONES
+function agregarProductos() {
+    let numeroVehiculos = parseInt(prompt("¿Cuantos vehículos desea cotizar?"));
+        for (let index = 0; index < numeroVehiculos; index++) {
+            let marca = prompt("Ingrese la marca del vehículo");
+            let modelo = prompt("Ingrese el modelo del vehículo");
+            let estado = prompt("Ingrese el estado del vehículo");
+            let vehiculoCotizado = new Producto (marca, modelo, estado);
+            infoCotizacion.push(vehiculoCotizado);
+        }
+        return infoCotizacion;
+}
+
+function mostrarCotizaciones(infoCotizacion){
+    console.log(infoCotizacion)
+    return infoCotizacion
+}
 
 function userName(){
     alert("¡Bienvenido! a 'Sosa Seguros'");
@@ -16,15 +36,12 @@ function userName(){
     while (nombre === ''){
         nombre = prompt("Ingrese su nombre:");
     }
-
     alert("Hola " + nombre.toUpperCase() +"\nTe voy a peticionar que me indiques la marca del vehículo a asegurar");
     return nombre;
 }
 
 function mostrarMenu(){
-
     let opcion;
-
     while(opcion !== "ESC"){
         opcion = parseInt(prompt("Bienvenido, seleccione una opción:\n1) Volkswagen\n2) Chevrolet\n3) Fiat\n4) Renault"));
         switch (opcion){
@@ -59,57 +76,63 @@ function cotizacion(marca, dinero, nombre){
 
 function formaPago(){
     alert("Seleccione la forma de pago:");
-
     let pago;
-
     while(pago !== "ESC"){
         pago = parseInt(prompt("1) Efectivo\n2) Transferencia\n3) Tarjeta"));
         switch(pago) {
             case 1:
-                alert(`El precio final es de ${dinero}`);
+                    alert(`El precio final es de ${dinero}`)
+                    agregarCotizacion();
                 return "Efectivo";
             case 2:
-                alert(`El precio es de ${dinero}`);
-                
-                formula3(dinero, formula1(dinero, 0.10));
+                    alert(`El precio es de ${dinero}`);
+                        resta(dinero, descuento(dinero, 0.10));
+                        agregarCotizacion();
                 return "Descuento del 10%";
             case 3:
-                alert(`El precio es de ${dinero}`);
-                formula4(dinero, formula2(dinero, 0.10))
+                    alert(`El precio es de ${dinero}`);
+                        suma(dinero, recargo(dinero, 0.10))
+                        agregarCotizacion();
                 return "Recargo del 10%";
         }
     }
 }
 
-function formula1(valor1, valor2){
+function agregarCotizacion(){
+    let nuevaCotizacion;
+    while(nuevaCotizacion !== 2){
+        nuevaCotizacion = parseInt(prompt("¿Desea cotizar otro vehiculo?\nSeleccione una numero:\n1)Si\n2)No"));
+        switch(nuevaCotizacion){
+            case 1:
+                mostrarCotizaciones(agregarProductos());
+                return
+            case 2:
+                alert("¡Gracias por su cotización en Sosa Seguros, que tenga un buen día!");
+                break;
+        }
+    }
+}
 
+function descuento(valor1, valor2){
     let descuento = valor1 * valor2;
-
-    alert(`Tienes un descuento de ${descuento} por seleccionar este forma de pago`);
-
+        alert(`Tienes un descuento de ${descuento} por seleccionar este forma de pago`);
     return descuento;
 }
-function formula2(valor1, valor2){
 
+function recargo(valor1, valor2){
     let recargo = valor1 * valor2
-
-    alert(`Tienes un recargo de ${recargo} por seleccionar este forma de pago`);
-
+        alert(`Tienes un recargo de ${recargo} por seleccionar este forma de pago`);
     return recargo;
 }
-function formula3(valor1, valor2){
 
+function resta(valor1, valor2){
     let precioFinal = valor1 - valor2;
-
-    alert(`El precio final seria de ${precioFinal}`);
-
+        alert(`El precio final seria de ${precioFinal}`);
     return precioFinal;
 }
-function formula4(valor1, valor2){
 
+function suma(valor1, valor2){
     let precioFinal = valor1 + valor2;
-
-    alert(`El precio final seria de ${precioFinal}`);
-
+        alert(`El precio final seria de ${precioFinal}`);
     return precioFinal;
 }
