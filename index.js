2d36cb97881f7606f2ad16cc74db39a74de64cb7
Nico Sosa <sosanicolasdaniel00@gmail.com>
2022-07-04 22:32:10 -0300

    Primera-PreEntrega

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 // SIMULADOR INTERACTIVO
 class Producto {
-    constructor(marca, modelo, estado) {
-        this.marca = marca.toUpperCase();
-        this.modelo = modelo;
-        this.estado = estado;
+    constructor(marca, precio) {
+        this.marca = marca;
+        this.precio = precio;
     }
 }
+
+const auto1 = new Producto("Volkswagen", 5000);
+const auto2 = new Producto("Chevrolet", 4500);
+const auto3 = new Producto("Fiat", 4000);
+const auto4 = new Producto("Renault", 3500);
+
+const filtrado = [];
+let datosDeSeguro = 0;
 let infoCotizacion = [];
+let precioCotizacion = [];
 let nombreCliente = userName();
-let marcaAutomotor = mostrarMenu();
-let dinero = precios(marcaAutomotor);
-cotizacion(marcaAutomotor, dinero, nombreCliente);
-formaPago();
-
-function agregarProductos() {
-    let numeroVehiculos = parseInt(prompt("¿Cuantos vehículos desea cotizar?"));
-        for (let index = 0; index < numeroVehiculos; index++) {
-            let marca = prompt("Ingrese la marca del vehículo");
-            let modelo = prompt("Ingrese el modelo del vehículo");
-            let estado = prompt("Ingrese el estado del vehículo");
-            let vehiculoCotizado = new Producto (marca, modelo, estado);
-            infoCotizacion.push(vehiculoCotizado);
-        }
-        return infoCotizacion;
-}
-
-function mostrarCotizaciones(infoCotizacion){
-    console.log(infoCotizacion)
-    return infoCotizacion
-}
+let dinero = 0;
+procesos();
+filtrarResultado();
 
 function userName(){
     alert("¡Bienvenido! a 'Sosa Seguros'");
@@ -43,31 +33,41 @@ function userName(){
 function mostrarMenu(){
     let opcion;
     while(opcion !== "ESC"){
-        opcion = parseInt(prompt("Bienvenido, seleccione una opción:\n1) Volkswagen\n2) Chevrolet\n3) Fiat\n4) Renault"));
+        opcion = parseInt(prompt("Seleccione una opción:\n1) Volkswagen\n2) Chevrolet\n3) Fiat\n4) Renault"));
         switch (opcion){
             case 1:
-                return "Volkswagen";
+                infoCotizacion.push(auto1.marca);
+                precioCotizacion.push(auto1.precio);
+                dinero = 5000;
+                filtrado.push(auto1);
+                return;
             case 2:
-                return "Chevrolet";
+                infoCotizacion.push(auto2.marca);
+                precioCotizacion.push(auto2.precio);
+                dinero = 4500;
+                filtrado.push(auto2);
+                return;
             case 3:
-                return "Fiat";
+                infoCotizacion.push(auto3.marca);
+                precioCotizacion.push(auto3.precio);
+                dinero = 4000;
+                filtrado.push(auto3);
+                return;
             case 4:
-                return "Renault";
+                infoCotizacion.push(auto4.marca);
+                precioCotizacion.push(auto4.precio);
+                dinero = 3500;
+                filtrado.push(auto4);
+                return;
         }
     }    
 }
 
-function precios(opcion){
-    if (opcion === "Volkswagen"){
-        return 5000;
-    }else if (opcion === "Chevrolet"){
-        return 4500;
-    }else if (opcion === "Fiat"){
-        return 4000;
-    }else if (opcion === "Renault"){
-        return 3500;
-    }
+function filtrarResultado(){
+    const resultado = filtrado.filter((filtrado) => filtrado.marca.includes("Volkswagen"));
+    console.log(resultado);
 }
+
 
 function cotizacion(marca, dinero, nombre){
     alert(
@@ -75,24 +75,22 @@ function cotizacion(marca, dinero, nombre){
 }
 
 function formaPago(){
+    const pagos =["Efectivo","Transferencia","Tarjeta"];
     alert("Seleccione la forma de pago:");
     let pago;
     while(pago !== "ESC"){
-        pago = parseInt(prompt("1) Efectivo\n2) Transferencia\n3) Tarjeta"));
+        pago = parseInt(prompt("1)" + pagos[0] +"\n2)" + pagos[1] + "\n3)" + pagos[2]));
         switch(pago) {
             case 1:
-                    alert(`El precio final es de ${dinero}`)
-                    agregarCotizacion();
+                    alert(`El precio final es de ${dinero}`);
                 return "Efectivo";
             case 2:
-                    alert(`El precio es de ${dinero}`);
+                    alert(`El precio final es de ${dinero}`);
                         resta(dinero, descuento(dinero, 0.10));
-                        agregarCotizacion();
                 return "Descuento del 10%";
             case 3:
-                    alert(`El precio es de ${dinero}`);
-                        suma(dinero, recargo(dinero, 0.10))
-                        agregarCotizacion();
+                    alert(`El precio final es de ${dinero}`);
+                        suma(dinero, recargo(dinero, 0.10));
                 return "Recargo del 10%";
         }
     }
@@ -104,8 +102,8 @@ function agregarCotizacion(){
         nuevaCotizacion = parseInt(prompt("¿Desea cotizar otro vehiculo?\nSeleccione una numero:\n1)Si\n2)No"));
         switch(nuevaCotizacion){
             case 1:
-                mostrarCotizaciones(agregarProductos());
-                return
+                procesos();
+                return;
             case 2:
                 alert("¡Gracias por su cotización en Sosa Seguros, que tenga un buen día!");
                 break;
@@ -120,7 +118,7 @@ function descuento(valor1, valor2){
 }
 
 function recargo(valor1, valor2){
-    let recargo = valor1 * valor2
+    let recargo = valor1 * valor2;
         alert(`Tienes un recargo de ${recargo} por seleccionar este forma de pago`);
     return recargo;
 }
@@ -135,4 +133,19 @@ function suma(valor1, valor2){
     let precioFinal = valor1 + valor2;
         alert(`El precio final seria de ${precioFinal}`);
     return precioFinal;
+}
+
+function procesos(){
+    mostrarMenu();
+    cotizacion(infoCotizacion, precioCotizacion, nombreCliente);
+    formaPago();
+    concatenacion();
+    infoCotizacion = [];
+    precioCotizacion = [];
+    agregarCotizacion();
+}
+
+function concatenacion(){
+    datosDeSeguro = infoCotizacion.concat(precioCotizacion);
+        console.log(datosDeSeguro);
 }
